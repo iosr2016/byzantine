f982c013df16b7c903d206b7da7159d435c4807c
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-11-10 00:05:21 +0100

    Reorganize requires

---
+++ lib/byzantine/handlers.rb
@@ -0,0 +1,9 @@
+require 'byzantine/handlers/base_handler'
+require 'byzantine/handlers/accept_handler'
+require 'byzantine/handlers/get_handler'
+require 'byzantine/handlers/prepare_handler'
+require 'byzantine/handlers/promise_handler'
+require 'byzantine/handlers/request_handler'
+
+module Handlers
+end
---
+++ lib/byzantine/messages.rb
@@ -0,0 +1,9 @@
+require 'byzantine/messages/base_message'
+require 'byzantine/messages/accept_message'
+require 'byzantine/messages/get_message'
+require 'byzantine/messages/prepare_message'
+require 'byzantine/messages/promise_message'
+require 'byzantine/messages/request_message'
+
+module Messages
+end
