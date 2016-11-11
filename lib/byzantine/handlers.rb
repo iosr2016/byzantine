5566ecab39b3acaca36159ff760f4c35b1caa721
Maciej Gawel <m.gawel@hotmail.com>
2016-11-11 18:15:39 +0100

    Add missing handler require

--- lib/byzantine/handlers.rb
+++ lib/byzantine/handlers.rb
@@ -1,6 +1,7 @@
 require 'byzantine/handlers/base_handler'
 require 'byzantine/handlers/accept_handler'
 require 'byzantine/handlers/get_handler'
+require 'byzantine/handlers/nack_handler'
 require 'byzantine/handlers/prepare_handler'
 require 'byzantine/handlers/promise_handler'
 require 'byzantine/handlers/request_handler'
