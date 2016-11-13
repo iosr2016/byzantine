c72f3e1d6f6e90c5b9142e5ba81d1130b3103b6a
Maciej Gawel <m.gawel@hotmail.com>
2016-11-13 18:19:01 +0100

    Add fault tolerance

--- lib/byzantine/cli.rb
+++ lib/byzantine/cli.rb
@@ -5,27 +5,33 @@ module Byzantine
     HOSTS_FILE_PATH = '.byzantine.hosts'.freeze
 
     desc 'start', 'Starts data store instance'
-    method_option :port,        default: 4_000,           type: :numeric
-    method_option :host,        default: 'localhost',     type: :string
-    method_option :hosts_file,  default: HOSTS_FILE_PATH, type: :string
+    method_option :host,            type: :string,  default: 'localhost'
+    method_option :port,            type: :numeric, default: 4_000
+    method_option :fault_tolerance, type: :numeric, default: 0
+    method_option :hosts_file,      type: :string
 
     def start
       runner = Runner.new
-
-      runner.configure do |config|
-        config.host         = options[:host]
-        config.client_port  = options[:port]
-        config.queue_port   = options[:port] + 1
-        config.node_urls    = read_hosts(options[:hosts_file])
-      end
+      config_runner runner
 
       runner.start
     end
 
     private
 
+    def config_runner(runner)
+      runner.configure do |config|
+        config.host             = options[:host]
+        config.client_port      = options[:port]
+        config.queue_port       = options[:port] + 1
+        config.fault_tolerance  = options[:fault_tolerance]
+        config.node_urls        = read_hosts(options[:hosts_file])
+      end
+    end
+
     def read_hosts(hosts_file)
-      return [] unless File.exist?(hosts_file)
+      return [] unless hosts_file
+
       File.readlines(hosts_file).map(&:strip)
     end
   end
--- lib/byzantine/configuration.rb
+++ lib/byzantine/configuration.rb
@@ -1,13 +1,14 @@
 module Byzantine
   class Configuration
-    attr_accessor :store, :host, :client_port, :queue_port, :node_urls
+    attr_accessor :store, :host, :client_port, :queue_port, :node_urls, :fault_tolerance
 
     def initialize
-      @store        = Stores::PStore
-      @host         = 'localhost'
-      @client_port  = 4_000
-      @queue_port   = 4_001
-      @node_urls    = []
+      @store            = Stores::PStore
+      @host             = 'localhost'
+      @client_port      = 4_000
+      @queue_port       = 4_001
+      @node_urls        = []
+      @fault_tolerance  = 0
     end
   end
 end
